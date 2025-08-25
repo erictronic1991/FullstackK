ba2fb629691a237cfd071d1223f43b06801bec98
erictronic1991 <relleve18eric@gmail.com>
2025-08-25 19:02:00 +0800

    !

--- server.js
+++ server.js
@@ -120,7 +120,7 @@ app.post('/tasks', authenticateToken, (req, res) => {
 
 // Start server
 app.get('/', (req, res) => {
-    res.send('Welcome to the backend server');
+    res.send('Welcome to the backend server!');
 });
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
